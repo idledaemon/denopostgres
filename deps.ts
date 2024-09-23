916eba0938d317c2c849b806cac7832fe8c004c7
idledaemon <showycloud@gmail.com>
2024-09-23 13:31:23 +0800

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -1,18 +1,18 @@
-export * as base64 from "https://deno.land/std@0.214.0/encoding/base64.ts";
-export * as hex from "https://deno.land/std@0.214.0/encoding/hex.ts";
-export { parse as parseDate } from "https://deno.land/std@0.214.0/datetime/parse.ts";
-export { BufReader } from "https://deno.land/std@0.214.0/io/buf_reader.ts";
-export { BufWriter } from "https://deno.land/std@0.214.0/io/buf_writer.ts";
-export { copy } from "https://deno.land/std@0.214.0/bytes/copy.ts";
-export { crypto } from "https://deno.land/std@0.214.0/crypto/crypto.ts";
-export { delay } from "https://deno.land/std@0.214.0/async/delay.ts";
+export * as base64 from "https://raw.githubusercontent.com/denoland/deno_std/0.214.0/encoding/base64.ts";
+export * as hex from "https://raw.githubusercontent.com/denoland/deno_std/0.214.0/encoding/hex.ts";
+export { parse as parseDate } from "https://raw.githubusercontent.com/denoland/deno_std/0.214.0/datetime/parse.ts";
+export { BufReader } from "https://raw.githubusercontent.com/denoland/deno_std/0.214.0/io/buf_reader.ts";
+export { BufWriter } from "https://raw.githubusercontent.com/denoland/deno_std/0.214.0/io/buf_writer.ts";
+export { copy } from "https://raw.githubusercontent.com/denoland/deno_std/0.214.0/bytes/copy.ts";
+export { crypto } from "https://raw.githubusercontent.com/denoland/deno_std/0.214.0/crypto/crypto.ts";
+export { delay } from "https://raw.githubusercontent.com/denoland/deno_std/0.214.0/async/delay.ts";
 export {
   bold,
   rgb24,
   yellow,
-} from "https://deno.land/std@0.214.0/fmt/colors.ts";
+} from "https://raw.githubusercontent.com/denoland/deno_std/0.214.0/fmt/colors.ts";
 export {
   fromFileUrl,
   isAbsolute,
   join as joinPath,
-} from "https://deno.land/std@0.214.0/path/mod.ts";
+} from "https://raw.githubusercontent.com/denoland/deno_std/0.214.0/path/mod.ts";
